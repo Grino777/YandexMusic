a33aec85e7e4e09a958b713fbf3a54990c4c86c1
Grino777 <bars_B_P@mail.ru>
2024-04-21 16:42:15 +0300

    Update project

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
+# coding=utf-8
+
 """Download music app"""
 
 import asyncio
 import os
 import re
-
+import sys
 from asyncio import Task
-from typing import Dict, List, Union
+from typing import Dict, List
+
 from tqdm import tqdm
 from transliterate import translit
-from yandex_music import ClientAsync, Playlist, TrackShort
+from yandex_music import ClientAsync, Playlist, TrackShort, TracksList
+from yandex_music.exceptions import (
+    InvalidBitrateError,
+    TimedOutError,
+    UnauthorizedError,
+)
 from yandex_music.track.track import Track
-from yandex_music.exceptions import InvalidBitrateError, TimedOutError, UnauthorizedError
 
 from utils.client import get_client
-from utils.users import yandex_clients
-from utils.users import YandexUser
+from utils.users import YandexUser, yandex_users
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class YandexMusicDownloader:
@@ -25,12 +33,12 @@ class YandexMusicDownloader:
     ROOT_DIRECTORY = os.path.dirname((os.path.realpath(__file__)))
     NAME_MUSIC_FOLDER = "MUSIC"
 
-    def __init__(self, client: ClientAsync, user: YandexUser):
+    def __init__(self, client: ClientAsync, user: YandexUser) -> None:
         self.client = client
         self.user_uid = user.uid
         self.user_name = user.name.capitalize()
         self.user_directory = self._get_user_directory()
-        self.user_playlists: List[Dict[str, Union[Playlist, str]]] = []
+        self.user_playlists: List[Dict[str, Playlist]] = []
 
     def _get_user_directory(self) -> str:
         path = os.path.join(self.ROOT_DIRECTORY, self.NAME_MUSIC_FOLDER, self.user_name)
@@ -43,37 +51,43 @@ class YandexMusicDownloader:
         for i in range(0, len(tracks_list), chunk_size):
             yield tracks_list[i: i + chunk_size]
 
-    def _create_username_folder(self):
+    def _create_username_folder(self) -> None:
         path = os.path.join(self.ROOT_DIRECTORY, self.NAME_MUSIC_FOLDER, self.user_name)
         if not os.path.exists(path):
             os.makedirs(path)
 
-    def _create_playlists_folders(self):
+    def _create_playlists_folders(self) -> None:
         """Создание папок по названию плейлистов"""
         user_directory = self.user_directory
 
         for playlist_item in self.user_playlists:
-            path = os.path.join(user_directory, playlist_item.get('title'))
+            path = os.path.join(user_directory, playlist_item.get("title"))  # type: ignore
             if not os.path.exists(path):
                 os.mkdir(path)
 
-    async def _get_playlists_user(self):
+    async def _get_the_favorite_playlist(self):
+        """Получить список треков с отметкой 'Мне нравится'"""
+
+        favorite_playlist: TracksList | None = await self.client.users_likes_tracks(
+            user_id=self.user_uid
+        )
+        tracks_list: List[TrackShort] = favorite_playlist.tracks  # pyright: ignore[reportOptionalMemberAccess]
+        print(tracks_list)
+
+    async def _get_playlists_user(self) -> None:
         """Получить список плейлистов пользователя"""
 
         user_playlists = await self.client.users_playlists_list(user_id=self.user_uid)
 
         if len(user_playlists) > 0:
-
             for playlist in user_playlists:
                 old_title = str(playlist.title)
                 title = old_title.replace("\xa0", " ")
                 title = translit(title, language_code="ru", reversed=True)
 
-                self.user_playlists.append(
-                    {"title": title, "playlist": playlist}
-                )
+                self.user_playlists.append({"title": title, "playlist": playlist})
         else:
-            print('У пользователя нет плейлисов либо они скрыты!')
+            print("У пользователя нет плейлистов либо они скрыты!")
 
     def _check_current_directory(self):
         """Проверка директории на наличие корневой папки music"""
@@ -89,8 +103,7 @@ class YandexMusicDownloader:
 
             tracks_list: List[TrackShort] = []
 
-            path = os.path.join(self.user_directory, playlist_item["title"])
-            # path = playlist_item["path"]
+            path = os.path.join(self.user_directory, playlist_item["title"])  # type: ignore
             playlist: Playlist = playlist_item.get("playlist")  # type:ignore
             tracks_list.extend(await playlist.fetch_tracks_async())
             tracks: List[Track | None] = [item.track for item in tracks_list]
@@ -106,10 +119,10 @@ class YandexMusicDownloader:
                 ]
                 await asyncio.gather(*coro_tracks)
 
-    async def _download_tracks(self, track: Track, path: str = ""):
+    async def _download_tracks(self, track: Track, path: str = "") -> None:
         """Загрузка трека"""
 
-        error_symbols = "[\/:*?\"<>\|+]"
+        error_symbols = r'[\\/:*?"<>\|+]'
 
         filename = f"{track.artists[0].name} - {track.title}.mp3"
         filename = re.sub(error_symbols, "", filename)
@@ -119,28 +132,29 @@ class YandexMusicDownloader:
 
         if not os.path.exists(track_path):
             try:
-
                 result = await track.download_async(
                     filename=track_path, codec="mp3", bitrate_in_kbps=320
                 )
             except InvalidBitrateError:
                 result = await track.download_async(
-                    filename=track_path, codec="mp3", bitrate_in_kbps=192,
+                    filename=track_path,
+                    codec="mp3",
+                    bitrate_in_kbps=192,
                 )
             except TimedOutError:
                 await asyncio.sleep(1)
                 result = await track.download_async(
-                    filename=track_path, codec="mp3", bitrate_in_kbps=192,
+                    filename=track_path,
+                    codec="mp3",
+                    bitrate_in_kbps=192,
                 )
             except UnauthorizedError:
                 pass
-            except Exception as e:
-                print(e)
 
             return result
         return result
 
-    async def init(self):
+    async def init(self) -> None:
         """Инициализация"""
 
         self._check_current_directory()
@@ -150,42 +164,89 @@ class YandexMusicDownloader:
         await self._download_tracks_from_the_playlist()
 
 
-def check_user_uid() -> YandexUser | None:
-    """Получить user data"""
+class App():
+    CHOICES = {
+        1: 'Добавить пользователя в users_list',
+        2: 'Выбрать пользователя для скачивания плейлистов',
+    }
 
-    try:
-        user_id = int(input('Введите ID: '))
+    def _check_user_uid(self) -> YandexUser | None:
+        """Получить user data"""
 
-        for client in yandex_clients:
+        user_id = input("Введите ID: ")
+
+        if user_id == 'q':
+            sys.exit()
+
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            print("Вы ввели неправильный ID или ввели строку!")
+
+        for client in yandex_users:
             if user_id == client.id:
-                return client
-    except:
-        print("Вы ввели неправильный ID или ввели строку!")
+                result = client
 
+        return result
 
-def run_user_selection() -> YandexUser:
-    """Функция запуска выбора ID пользователя"""
+    def _get_user(self):
+        """Функция запуска выбора пользователя"""
 
-    print('Выберете ID пользователя у которого хотите скачать плейлисты:')
+        os.system('cls')
 
-    for client in yandex_clients:
-        print(f'{client.name.capitalize()}: {client.id}', sep='\n')
-    print()
+        print("Выберете ID пользователя у которого хотите скачать плейлисты:")
 
-    while True:
-        user = check_user_uid()
-        if user:
+        for client in yandex_users:
+            print(f"{client.name.capitalize()}: {client.id}", sep="\n")
+        print()
+
+        while True:
+            user = self._check_user_uid()
+            if user:
+                return user
+
+    # def run_user_selection(self) -> YandexUser:
+
+    def run_user_selection(self) -> YandexUser:
+        """Функция запуска выбора пользователя"""
+
+        for choice_id, choice in self.CHOICES.items():
+            print(f'{choice_id}: {choice}', sep='\n')
+
+        user_choice = input('Выберете действие: ')
+
+        try:
+            user_choice = int(user_choice)
+
+            if user_choice not in self.CHOICES:
+                raise ValueError
+
+        except ValueError:
+            print('Выбрали неправильное значение!\n')
+            self.run_user_selection()
+
+        if user_choice == 1:
+            pass
+        if user_choice == 2:
+            user = self._get_user()
             return user
+        if user_choice == 'q':
+            sys.exit()
 
 
 async def main():
     """Main def"""
 
-    client: ClientAsync = await get_client()
-    user = run_user_selection()
+    app = App()
 
-    app = YandexMusicDownloader(client=client, user=user)
-    await app.init()
+    user = app.run_user_selection()
+
+    if user:
+        client: ClientAsync = await get_client()
+
+        downloader = YandexMusicDownloader(client=client, user=user)
+        await downloader.init()
+        # await app._get_the_favorite_playlist()
 
     print("DONE!")
 
--- utils/users.py
+++ utils/users.py
@@ -1,11 +1,39 @@
-from config import CLIENTS
+import json
+import os
+
+from config import ROOT_DIR
 from pydantic import BaseModel
 
-clients = CLIENTS
 
 class YandexUser(BaseModel):
     id: int
     name: str
     uid: int
 
-yandex_clients = [YandexUser(**client) for client in clients]
+
+def get_users():
+    path = os.path.join(ROOT_DIR, 'users_list', 'users_list.json')
+
+    with open(path, 'r', encoding='utf-8') as file:
+        users = json.load(file)
+
+    return users
+
+
+def add_user():
+    path = os.path.join(ROOT_DIR, 'users_list', 'users_list.json')
+
+    with open(path, 'w', encoding='utf-8') as file:
+        users = json.load(file)
+
+
+
+def converting_users(users: list):
+    users = [YandexUser(**user) for user in users]
+
+    return users
+
+
+yandex_users = get_users() if len(get_users()) == 0 else []
+
+yandex_users = converting_users(yandex_users)
